55aace270cc3ccef53c4cc8c65a77a174fa53ff2
Ganappriyan <ganappriyanc@gmail.com>
2022-05-12 17:50:50 +0530

    Test

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Virtual-Treasure-Hunt
+
+Django Install: 
+
+python -m pip install Django
+
+
+Heroku Installination: 
+
+pip install dj-database-url
+pip install django-heroku
+pip install gunicorn
+pip install psycopg2
+pip install python-decouple
+pip install whitenoise
--- server/venv/bin/activate.fish
+++ server/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ganappriyan/VSCodeProjects/Virtual-Treasure-Hunt/V2/server/venv"
+set -gx VIRTUAL_ENV "/home/ganappriyan/VSCodeProjects/Virtual-Treasure-Hunt/server/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
